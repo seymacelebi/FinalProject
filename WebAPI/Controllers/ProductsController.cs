86790bad7f8eefc975f84ed32707dfefd095cf12
Seyma CELEBI <seyma.celebi@st.uskudar.edu.tr>
2021-03-20 02:09:16 +0300

    11. dersin sonu

--- WebAPI/Controllers/ProductsController.cs
+++ WebAPI/Controllers/ProductsController.cs
@@ -23,14 +23,38 @@ namespace WebAPI.Controllers
         {
             _productService = productService;
         }
-
-        [HttpGet]
-        public List<Product> Get()
+        //isimlerle allians vermiş olduk
+        [HttpGet("getall")]
+        public IActionResult GetAll()
         {
-            IProductService productService = new ProductManager(new EfProductDal());
-            var result= productService.GetAll();
-            return result.Data;
+            var result = _productService.GetAll();
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
         }
+        [HttpGet("getbyid")]
+        public IActionResult GetById(int id)
+        {
+            var result = _productService.GetById(id);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpPost("add")]
+        public IActionResult Add(Product product)
+        {
+            var result = _productService.Add(product);
+            if (result.Success==true)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
     }
 }
- 
