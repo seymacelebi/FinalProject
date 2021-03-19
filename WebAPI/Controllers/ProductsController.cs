53373dd15894681ffe35f898f58e5b6fea87ac5c
Seyma CELEBI <seyma.celebi@st.uskudar.edu.tr>
2021-03-19 23:45:04 +0300

    11.ders 1.bölüm

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,35 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        //Loosely coupled
+        IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public List<Product> Get()
+        {
+            IProductService productService = new ProductManager(new EfProductDal());
+            var result= productService.GetAll();
+            return result.Data;
+        }
+    }
+}
+ 
